7e0def98969ade581800019b782de781bb57a876
Majid Hajiloo <majid.hajiloo@gmail.com>
2019-02-20 01:45:45 +0330

    cleaning

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from io import open
 from setuptools import setup
 
@@ -5,7 +8,7 @@ version = '1.2.2'
 
 
 def readme():
-    with open('README.rst', encoding='utf-8') as f:
+    with open('README.md', encoding='utf-8') as f:
         return f.read()
 
 
@@ -19,7 +22,9 @@ setup(name='persiantools',
           'Natural Language :: Persian',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
@@ -33,14 +38,13 @@ setup(name='persiantools',
           'Topic :: Utilities',
       ],
       keywords='jalali shamsi persian digits characters converter jalalidate '
-               'jalalidatetime date datetime',
+               'jalalidatetime date datetime jdatetime',
       url='https://github.com/mhajiloo/persiantools',
       author='Majid Hajiloo',
       author_email='majid.hajiloo@gmail.com',
       license='MIT',
       packages=['persiantools'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
+      tests_require=['pytest', 'pytest-cov'],
       install_requires=[],
       include_package_data=True,
       zip_safe=False)
