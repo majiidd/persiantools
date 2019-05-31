9c21420e312c55eb96d160481ae28adc8b30bacb
Majid Hajiloo <majid.hajiloo@gmail.com>
2019-05-31 17:32:06 +0430

    add python 3.8

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,7 @@ class TestUtils(TestCase):
     def test_replace(self):
         self.assertEqual(utils.replace("Persian Tools", {"Persian": "Parsi", " ": "_"}), "Parsi_Tools")
         self.assertEqual(utils.replace("آب بی فلسفه می‌خوردم", {"آب": "آآآب", " ": "_"}), "آآآب_بی_فلسفه_می‌خوردم")
-        self.assertEqual(utils.replace(u"ای بس که نباشیم و جهان خواهد بود", {"ای": "اایی", "خواهد": "خخووااههدد"}), "اایی بس که نباشیم و جهان خخووااههدد بود")
+        self.assertEqual(utils.replace(u"ای بس که نباشیم و جهان خواهد بود", {u"ای": u"اایی", u"خواهد": u"خخووااههدد"}), u"اایی بس که نباشیم و جهان خخووااههدد بود")
 
     def test_int(self):
         self.assertEqual(utils.check_int_field(100010001), 100010001)
